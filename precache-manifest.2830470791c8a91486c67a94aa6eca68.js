c719d5d9bbdd82025b71dc2e71a6432cd0f92a39
IBTech28 <isaiahbuchberger@gmail.com>
2019-04-03 21:13:46 -0500

    Updates

---
+++ precache-manifest.2830470791c8a91486c67a94aa6eca68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "affa86a2af03173f6944",
+    "url": "/robofriends/static/js/main.8a1ee380.chunk.js"
+  },
+  {
+    "revision": "457d7418e1d7b68f3289",
+    "url": "/robofriends/static/js/2.8f6899b5.chunk.js"
+  },
+  {
+    "revision": "affa86a2af03173f6944",
+    "url": "/robofriends/static/css/main.b3480330.chunk.css"
+  },
+  {
+    "revision": "457d7418e1d7b68f3289",
+    "url": "/robofriends/static/css/2.53afcda4.chunk.css"
+  },
+  {
+    "revision": "16519026ec357e1c3fe379e5ae2907e0",
+    "url": "/robofriends/index.html"
+  }
+];
